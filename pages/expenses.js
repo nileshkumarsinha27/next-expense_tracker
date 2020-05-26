2d667be758342c92d3f53b005b7a8bc019e63641
nileshkumarsinha27 <nileshsinha.sinha0@gmail.com>
2020-05-26 21:59:21 +0530

    revert changes

--- pages/expenses.js
+++ pages/expenses.js
@@ -1,10 +1,10 @@
 /**
- * Importing the dependencies	 * Importing the dependencies
+ * Importing the dependencies
  */ import Layout from '../components/layout/Layout';
 import AddExpense from '../components/addExpense/AddExpense';
 
 /**
- * The index route of the application	 * The index route of the application
+ * The index route of the application
  */
 const Expenses = () => (
   <Layout>
